180780df40ba7e0723f689617ead8e676e687d69
ws34ed5r6y78unoip <49279661+ws34ed5r6y78unoip@users.noreply.github.com>
2019-04-09 10:00:26 -0300

    Update ExpLog.java

--- src/main/java/com/grupo/ExpLog.java
+++ src/main/java/com/grupo/ExpLog.java
@@ -1,13 +1,27 @@
-package com.grupo;
+import java.util.ArrayList;
+import java.lang.IllegalArgumentException;
+public class ExpLog{
+    public static double exponencial(double x,double fim){
+        ArrayList<Double> aux = new ArrayList<>();
+        double e;
+	int i;
+        for(i = 0;i<fim;i++){
+            e = (Math.pow(x,i))/Util.fatorial(i);
+            aux.add(e);
+        }
+        return Util.somatorio(0,i,aux);
+    }
 
-/**
- * 
- *
- */
-public class ExpLog
-{
-    public static void ExpLog()
-    {
+    public static double logaritmo(double x,double fim){
         
+        if(-1 > x || x < 1) throw new IllegalArgumentException();
+        double ln;  
+        ArrayList<Double> aux = new ArrayList<>();
+	    int i;
+        for(i = 0;i<fim;i++){
+            ln = ((Math.pow(-1,i))/(i+1))*(Math.pow(x,i+1));
+            aux.add(ln);
+        }
+        return  Util.somatorio(0,i,aux);
     }
 }

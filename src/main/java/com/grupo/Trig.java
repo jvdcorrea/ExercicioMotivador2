1d0f95fca23450c02f15f2d41556e53778bd149b
ws34ed5r6y78unoip <49279661+ws34ed5r6y78unoip@users.noreply.github.com>
2019-04-09 09:59:58 -0300

    Update Trig.java

--- src/main/java/com/grupo/Trig.java
+++ src/main/java/com/grupo/Trig.java
@@ -1,13 +1,31 @@
-package com.grupo;
+import java.util.ArrayList;
+package com.bcopstein.FuncoesST;
 
-/**
- * 
- *
- */
-public class Trig
-{
-    public static void Trig()
-    {
-        
+public class Trig{
+
+
+    public static double cos(int x, int fim){
+        ArrayList<Double> aux = new ArrayList<>();
+        double e;
+	    int i;
+        for(i = 0;i<fim;i++){
+            e = ((Math.pow(-1,i))/Util.fatorial(2*i) * Math.pow(x,2*i));
+            aux.add(e);
+        }
+        return Util.somatorio(0,i,aux);
     }
+
+
+    public static double sen(int x, int fim){
+        ArrayList<Double> aux = new ArrayList<>();
+        double e;
+	    int i;
+        for(i = 0;i<fim;i++){
+            e = ((Math.pow(-1,i))/Util.fatorial((2*i)+1) * Math.pow(x,(2*i)+1));
+            aux.add(e);
+        }
+        return Util.somatorio(0,i,aux);
+    }
+
+
 }
